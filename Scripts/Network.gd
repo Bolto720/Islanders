a857b3ba6721b6d61e2b3780a9a4223d6b86fc7e
Bolto720 <bolto720@gmail.com>
2019-09-24 23:43:09 +0100

    Fixed render bug, client updated when player enters and leaves island.

--- Scripts/Network.gd
+++ Scripts/Network.gd
@@ -4,7 +4,6 @@ onready var manager = get_node("/root/Manager")
 
 var connection = null
 var peerstream = null
-var test = null
 
 var player
 var playerPosX
@@ -25,9 +24,8 @@ func _ready():
 func _process(delta):
 	if connection.is_connected_to_host():
 		if connection.get_available_bytes() > 0:
-			test = connection.get_var()
-			#print(test)
-			server_command(test)
+			var command = connection.get_var()
+			server_command(command)
 
 func server_command(command):
 	var res = {}
@@ -47,11 +45,14 @@ func server_command(command):
 		if res.Result == false:
 			get_tree().get_root().get_node("Login/Result").text = "Unable to register"
 		else:
-			get_tree().get_root().get_node("Login/Result").text = "Registration complete, you can now login"
+			get_tree().get_root().get_node("Login/Result").text = "Registration complete, you can now login"	
 	elif res.Command == "broadcast" || res.Command == "enter":
 		if !networkPlayers.has(res.Player):
 			add_network_player(res)
-		networkPlayers.get(res.Player).try_move(res.Pos_x, res.Pos_y)	
+			player.server_command({"Player": player._name, "Command": "move", "Pos_x": player.position.x, "Pos_y": player.position.y})
+		networkPlayers.get(res.Player).try_move(res.Pos_x, res.Pos_y)
+	elif res.Command == "quit":
+		remove_network_player(command)
 	elif res.Player == player._name:
 		player.server_command(command)
 
@@ -73,3 +74,7 @@ func add_network_player(packetData):
 	networkPlayer.create()
 	get_tree().get_root().get_node("Game").add_child(networkPlayer)
 	networkPlayers[networkPlayer._name] = networkPlayer
+	
+func remove_network_player(packetData):
+	networkPlayers[packetData.Player].get_parent().remove_child(networkPlayers[packetData.Player])
+	networkPlayers.erase(packetData.Player)
